61fba38f631983afa421af22bfda84e8de3ff732
Matesor333 <matus.illit@gmail.com>
2025-01-30 11:12:37 +0000

    getform stuff

--- lab_11/server.js
+++ lab_11/server.js
@@ -10,6 +10,12 @@ app.get('/test', function(req, res){
     var x = req.query.x; 
     var y = req.query.y; 
        res.send("X + Y="+(x+y)); 
-    }); 
+    });
+    app.get('/getform', function(req, res){ 
+      var name = req.query.name; 
+      var quest = req.query.quest; 
+         res.send("Hi "+name+" I am sure you will "+quest) ; 
+      }); 
     app.use(express.static('public'))
+
 app.listen(8080);
