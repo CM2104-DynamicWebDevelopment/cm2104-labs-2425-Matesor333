128214ec2ddbee854ee9feaf0ca8380fe07a0a8c
Matesor333 <matus.illit@gmail.com>
2025-01-30 11:25:59 +0000

    idk 2

--- lab_11/server.js
+++ lab_11/server.js
@@ -6,21 +6,26 @@ app.get('/', function(req, res){
 app.get('/test', function(req, res){ 
     res.send("this is route 2"); 
  });
- app.get('/add', function(req, res){ 
+app.get('/add', function(req, res){ 
     var x = req.query.x; 
     var y = req.query.y; 
        res.send("X + Y="+(x+y)); 
     });
-    app.get('/getform', function(req, res){ 
+app.get('/getform', function(req, res){ 
       var name = req.query.name; 
       var quest = req.query.quest; 
-         res.send("Hi "+name+" I am sure you will "+quest) ; 
+         res.send("Hi "+name+" I am sure you will "+quest); 
       }); 
-      app.post('/postform', function(req, res){ 
-         var name = req.body.name; 
-         var quest = req.body.quest; 
-            res.send("Hi "+name+" I am sure you will "+quest) ; 
-         });
+app.post('/postform', function(req, res){ 
+       var name = req.body.name; 
+      var quest = req.body.quest; 
+       res.send("Hi "+name+" I am sure you will "+quest); 
+      });
+      
+      app.use(function ( req, res, next) { 
+         res.send("This page does not exist!"); 
+     }) 
+      
     app.use(express.static('public'))
     app.use(express.urlencoded({extended:true}))
 
