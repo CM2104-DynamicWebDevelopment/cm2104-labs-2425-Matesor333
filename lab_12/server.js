f4ee3f1e46dd5f1e1f77750871b55edd4c2c39e5
Matesor333 <matus.illit@gmail.com>
2025-02-06 18:43:17 +0000

    AAAA

--- lab_12/server.js
+++ lab_12/server.js
@@ -5,4 +5,27 @@ var app = express();
 , function(req, res){
  res.send("Hello world! by express");
 });
+spotifyApi.clientCredentialsGrant().then( 
+    function (data) { 
+    console.log('The access token expires in ' + data.body['expires_in']); 
+    console.log('The access token is ' + data.body['access_token']); 
+    // Save the access token so that it's used in future calls 
+    spotifyApi.setAccessToken(data.body['access_token']);
+
+    },
+    function (err) { 
+        console.log( 
+        'Something went wrong when retrieving an access token', 
+        err.message 
+                ); 
+        });
+        async function getTracks(searchterm, res) { 
+            spotifyApi.searchTracks(searchterm) 
+            .then(function (data) { 
+            res.send(JSON.stringify(data.body)); 
+            }, function (err) { 
+            console.error(err); 
+            }); 
+            }
+            
 app.listen(8080);
