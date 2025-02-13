ff14e201d82e470170b80c153df49e2ae7f24717
Matesor333 <matus.illit@gmail.com>
2025-02-13 11:10:54 +0000

    7

--- lab_13/server.js
+++ lab_13/server.js
@@ -36,7 +36,7 @@ app.get('/all', function(req, res) {
    });
 
    app.use(express.urlencoded({extended:true}))
-   
+
    app.post('/quotes', function (req, res) {
     db.collection('quotes').insertOne(req.body, function(err, result) {
    if (err) throw err;
@@ -44,4 +44,19 @@ app.get('/all', function(req, res) {
       res.redirect('/')
     })
    })
+
+   app.post('/search', function(req, res) {
+    db.collection('quotes').find(req.body).toArray(function(err, result) {
+   if (err) throw err;
+   var output = "<h1>All the quotes</h1>";
+   for (var i = 0; i < result.length; i++) {
+        output += "<div>"
+        output += "<h3>" + result[i].name + "</h3>"
+        output += "<p>" + result[i].quote + "</p>"
+        output += "</div>"
+      }
+      res.send(output);
+    });
+   });
+  
   
