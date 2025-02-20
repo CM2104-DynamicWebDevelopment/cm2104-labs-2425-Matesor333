87fe1e0fc8bf936068b86d67e2189f4b37a51186
Matesor333 <matus.illit@gmail.com>
2025-02-20 10:38:37 +0000

    aaaaaa

--- lab_14/server.js
+++ lab_14/server.js
@@ -2,10 +2,10 @@
  // load the things we need
  var express = require('express');
  var app = express();
- /*// set the view engine to ejs
+ // set the view engine to ejs
  app.set('view engine', 'ejs');
  // use res.render to load up an ejs view file
- // index page 
+ /*// index page 
 app.get('/', function(req, res) {
     res.render('pages/index');
  });*/
