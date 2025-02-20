4e697a9415aff5fd107425c02ac55c4ac86ed8dd
Matesor333 <matus.illit@gmail.com>
2025-02-20 10:37:02 +0000

    5.2.1fix

--- lab_14/server.js
+++ lab_14/server.js
@@ -1,8 +1,8 @@
  // server.js
  // load the things we need
- /*var express = require('express');
+ var express = require('express');
  var app = express();
- // set the view engine to ejs
+ /*// set the view engine to ejs
  app.set('view engine', 'ejs');
  // use res.render to load up an ejs view file
  // index page 
