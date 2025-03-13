37ae9629014a397aadff970e2555feed89e01ead
Matesor333 <matus.illit@gmail.com>
2025-03-13 10:23:55 +0000

    3

--- lab_16/server.js
+++ lab_16/server.js
@@ -13,7 +13,10 @@ io.on('connection', function (socket) {
     console.log('a user connected'); 
     socket.on('disconnect', function () { 
         console.log('user disconnected'); 
-}); 
+    });
+    socket.on('chat message', function (msg) { 
+        console.log('message: ' + msg); 
+    });  
 });
 http.listen(8080, function(){ 
   console.log('listening on *:8080'); 
