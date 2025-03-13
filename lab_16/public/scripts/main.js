c037611806d5539c6eb0d6f9b5f1919a3b84f351
Matesor333 <matus.illit@gmail.com>
2025-03-13 11:02:59 +0000

    fix

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -9,16 +9,7 @@ $('#form').submit(function () {
     return false;  
     })
     socket.on('chat message', function(msg) { 
-        if(user==true){
-            $('#Username').append(+" User");
-            $('#messages').append("<li>"+msg+"</li>");
-            
-        } 
-        if(user==false){
-            $('#Username').append(+" ANother user");
-            $('#messages').append("<li>"+msg+"</li>");
-            
-        } 
-        window.scrollTo(0, document.body.scrollHeight);
-        user =false 
+        
+        $('#messages').append("<li>"+msg+"</li>"); 
+        window.scrollTo(0, document.body.scrollHeight); 
     });
